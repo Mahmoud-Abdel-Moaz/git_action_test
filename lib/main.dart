a85e30bb4b570ec1ed8e9824950b8a9fc3fb31da
Mahmoud Abdel Moaz <33752332+Mahmoud-Abdel-Moaz@users.noreply.github.com>
2024-01-05 11:01:02 +0200

    test supply init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -14,6 +15,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'client.env');
   await Firebase.initializeApp();
+
+// Elsewhere in your code
+//  FirebaseCrashlytics.instance.crash();
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   _setColorTheme();
   runApp(const MyApp());
 }
@@ -189,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: colorsTheme.fabColor,
-        onPressed: _incrementCounter,
+        onPressed: FirebaseCrashlytics.instance.crash /*_incrementCounter*/,
         tooltip: 'Increment',
         child: const Icon(
           Icons.add,
